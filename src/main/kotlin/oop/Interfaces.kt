9140ab798db0c94bad0efbf21ed21dbb2d0a466f
ValerieMbaka <valeriembaka0@gmail.com>
2023-03-21 11:33:33 +0300

    fourth commit

---
+++ src/main/kotlin/oop/Interfaces.kt
@@ -0,0 +1,18 @@
+package oop
+
+fun main(args: Array<String>) {
+    var sch1 = School()
+    sch1.motto()
+    sch1.mission()
+}
+interface A{
+    fun motto(){
+        println("This is our motto")
+    }
+}
+interface B{
+    fun mission(){
+        println("This is our mission")
+    }
+}
+class School: A, B
